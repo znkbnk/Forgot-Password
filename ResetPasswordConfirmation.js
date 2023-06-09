6fae2c14bee525f6aa4c96749cc05d30f97fc34e
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-09 23:29:57 +0100

    Create ResetPasswordConfirmation.js

---
+++ ResetPasswordConfirmation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ResetPasswordConfirmation() {
+  return (
+    <div>
+      <h2>Password Reset Confirmation</h2>
+      <p>
+        A password reset email has been sent to your email address. Please check
+        your inbox and follow the instructions to reset your password.
+      </p>
+    </div>
+  );
+}
+
+export default ResetPasswordConfirmation;
